35d446c6d32ed110a936871cd03947fd63f27937
Timothy Clark <timeeeee@gmail.com>
2015-12-08 13:08:04 -0600

    Added riders and events pages

---
+++ events.php
@@ -0,0 +1,9 @@
+<?php session_start(); ?>
+<html>
+  <body>
+    <?php include("menu.php"); ?>
+    <div>
+      <h1>Events Schedule</h1>
+    </div>
+  </body>
+</html>
---
+++ riders.php
@@ -0,0 +1,9 @@
+<?php session_start(); ?>
+<html>
+  <body>
+    <?php include("menu.php"); ?>
+    <div>
+      <h1>Riders</h1>
+    </div>
+  </body>
+</html>
