92a4dc645550a7e697ba50bfd09bf93878361322
Timothy Clark <timeeeee@gmail.com>
2015-12-08 00:06:26 -0600

    pretty-up the home page

--- home.php
+++ home.php
@@ -1,12 +1,20 @@
 <?php session_start(); ?>
 <html>
-<?php include("menu.php"); ?>
-<h1>GOTTA GO FAST home</h1>
-<div id="welcome">
-  <?php
-     if (isset($_SESSION['user'])) {
-     echo("Welcome, " . $_SESSION['user']);
-     }
-     ?>
-</div>
+  <link rel="stylesheet" type="text/css" href="home.css">
+  <body>
+    <?php include("menu.php"); ?>
+	<div id="banner"></div>
+    <?php
+      if (isset($_SESSION['user'])) {
+      echo("<div id='welcome'>");
+      echo("Welcome, " . $_SESSION['user']);
+      echo("</div>");
+      }
+      ?>
+    <h1 id="home">GOTTA GO FAST</h1>
+	<div id="content">
+	  <div id="blog">blog text here</div>
+	  <div id="sponsors">sponsors here</div>
+	</div>
+  </body>
 </html>
